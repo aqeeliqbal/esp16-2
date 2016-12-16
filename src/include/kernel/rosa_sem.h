7724cd23fdbae0595fbc3c6ccf2210cbdc49551a
ahasanbegovic <hasanbegovic.arman@gmail.com>
2016-12-16 15:20:05 +0100

    Test case solutions
    Made an untested function for checking if a task is in the reg list

--- src/include/kernel/rosa_sem.h
+++ src/include/kernel/rosa_sem.h
@@ -14,5 +14,7 @@ int ROSA_prvSemaphoreRegister(semHandle sem, tcb* task);
 int ROSA_prvSemaphoreUnregister(semHandle sem, tcb* task);
 
 int ROSA_prvForceGiveSemaphore(semHandle s, tcb* task);
+int ROSA_prvSemaphoreIsTakenByTask(semHandle s, tcb* task);
+int ROSA_prvSemCheckInReglist(semHandle s, tcb* task);
 
 #endif
